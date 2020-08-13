1470ee8d2f672aa5b327366c4a7d7f48fcfc08a7
Nicolay <59583646+pvpender@users.noreply.github.com>
2020-08-13 16:50:44 +0300

    Add files via upload

--- pizda_komuto.py
+++ pizda_komuto.py
@@ -4,32 +4,32 @@ import random
 
 
 def register(cb):
-	cb(KickRandomMod())
+    cb(KickRandomMod())
+
 
 class KickRandomMod(loader.Module):
-	"""Пизда кому-то"""
-	strings = {'name': 'PizdaKomuto'}
+    """Пизда кому-то"""
+    strings = {'name': 'PizdaKomuto'}
 
-	async def kickpizdacmd(self, event):
-		"""Используй .kickpizda, чтобы пиздануть случайное существо (может кикнуть тебя)."""
-		user = random.choice([i for i in await event.client.get_participants(event.to_id)])
+    async def kickpizdacmd(self, event):
+        """Используй .kickpizda, чтобы пиздануть случайное существо (может кикнуть тебя)."""
+        myId = await event.client.get_me(event.to_id)
+        user = random.choice([i for i in await event.client.get_participants(event.to_id)])
+        while myId.user_id == user.id:
+            user = random.choice([i for i in await event.client.get_participants(event.to_id)])
+        await event.edit('<b>Кто-то сейчас пизданется...</b>')
+        await sleep(3)
 
-		await event.edit('<b>Кто-то сейчас пизданется...</b>')
-		ti = await event.client.get_me(event.to_id)
-		await event.edit(f"{ti.user_id}")
-		await sleep(3)
+        # Попытка кика...
+        try:
+            await event.client.kick_participant(event.chat_id, user.id)
+            await sleep(0.5)
+        except:
+            await event.edit('<b>Я лох, у меня нет прав чтобы кикнуть это чмо</b>')
+            return
 
-		# Попытка кика...
-		try:
-			#await event.client.kick_participant(event.chat_id, user.id)
-			await sleep(0.5)
-		except:
-			await event.edit('<b>Я лох, у меня нет прав чтобы кикнуть это чмо</b>')
-			return
-
-		# Кто кикнут.
-		name = str(user.first_name)
-		name += " "+user.last_name if user.last_name else ''
-		#await event.edit(f"Бог модуля Флингер пизданул <a href=\"tg://user?id={user.id}\">{user.first_name}</a>, и он улетел нахуй!")
-		#ti = await event.client.get_me()
-		#await event.edit(ti)
+        # Кто кикнут.
+        name = str(user.first_name)
+        name += " " + user.last_name if user.last_name else ''
+        await event.edit(
+            f"Бог модуля Флингер пизданул <a href=\"tg://user?id={user.id}\">{user.first_name}</a>, и он улетел нахуй!")
