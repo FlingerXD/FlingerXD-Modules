636482e958473bd2b8f34c60c23f4276f9eb5880
Nicolay <59583646+pvpender@users.noreply.github.com>
2020-08-13 15:23:43 +0300

    Add files via upload

--- pizda_komuto.py
+++ pizda_komuto.py
@@ -15,7 +15,7 @@ class KickRandomMod(loader.Module):
 		user = random.choice([i for i in await event.client.get_participants(event.to_id)])
 
 		await event.edit('<b>Кто сейчас пизданется... Оно работает если что</b>')
-		ti = await client.get_me()
+		ti = await event.client.get_me()
 		await event.edit(ti)
 		await sleep(3)
 
