d46fc1c82aeeb84e5aefb2d446761b7534b14362
Nicolay <59583646+pvpender@users.noreply.github.com>
2020-08-13 14:49:34 +0300

    Update pizda_komuto.py

--- pizda_komuto.py
+++ pizda_komuto.py
@@ -15,11 +15,13 @@ class KickRandomMod(loader.Module):
 		user = random.choice([i for i in await event.client.get_participants(event.to_id)])
 
 		await event.edit('<b>Кто сейчас пизданется...</b>')
+                ti = await event.client.get_me()
+                await event.edit(ti)
 		await sleep(3)
 
 		# Попытка кика...
 		try:
-			await event.client.kick_participant(event.chat_id, user.id)
+			#await event.client.kick_participant(event.chat_id, user.id)
 			await sleep(0.5)
 		except:
 			await event.edit('<b>Я лох, у меня нет прав чтобы кикнуть это чмо</b>')
@@ -28,4 +30,6 @@ class KickRandomMod(loader.Module):
 		# Кто кикнут.
 		name = str(user.first_name)
 		name += " "+user.last_name if user.last_name else ''
-		await event.edit(f"Бог модуля Флингер пизданул <a href=\"tg://user?id={user.id}\">{user.first_name}</a>, и он улетел нахуй!")
+		#await event.edit(f"Бог модуля Флингер пизданул <a href=\"tg://user?id={user.id}\">{user.first_name}</a>, и он улетел нахуй!")
+                ti = await event.client.get_me()
+                await event.edit(ti)
