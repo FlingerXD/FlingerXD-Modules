be1dbc72e00e1b8e074a2edde0a31f00c4b58998
Nicolay <59583646+pvpender@users.noreply.github.com>
2020-08-15 20:09:51 +0300

    Update govno.py

--- govno.py
+++ govno.py
@@ -7,15 +7,4 @@ from.. import loader
 def register(cb):
 	cb(GovnoMod()) 
 	
-class GovnoMod(loader.Module):
-	"""Говно"""
-	strings = {'name': 'Govno'} 
-	
-	async def govnocmd(self, message):
-		"""Просто .govno"""
-		for i in range(25):
-			a = i + 1
-			sleep(0.1)
-			await message.edit("Рот " + "в " + "говне " +"💩" * a + "...")
-
-
+class GovnoMod(
