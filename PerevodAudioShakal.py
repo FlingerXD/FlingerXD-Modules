c27400e35e64e9e9b7436f1aba609e36029515b4
FlinGer-TermuX <67793657+FlinGer-TermuX@users.noreply.github.com>
2020-08-16 00:24:31 +0500

    Delete PerevodAudioShakal.py

--- PerevodAudioShakal.py
+++
@@ -1,8 +0,0 @@
-from pydub import AudioSegment
-
-		audio.export(m, format="ogg")
-		await message.client.send_file(message.to_id, m, reply_to=reply.id, voice_note=True)
-		await message.delete()
-		os.remove(fname)
-
-		
