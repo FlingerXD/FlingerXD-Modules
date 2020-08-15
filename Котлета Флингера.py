f59ca624b43c934ba31cc5fb5dec6145c1985bb7
Nicolay <59583646+pvpender@users.noreply.github.com>
2020-08-15 20:15:02 +0300

    Update Котлета Флингера.py

--- Котлета Флингера.py
+++ Котлета Флингера.py
@@ -1,56 +1,7 @@
 from telethon import events
 from .. import loader, utils
 import os
-import requests
-from PIL import Image,ImageFont,ImageDraw 
-import re
-import io
-from textwrap import wrap
-
-def register(cb):
-	cb(JacquesMod())
-	
-class JacquesMod(loader.Module):
-	"""Жаконизатор"""
-	strings = {
-		'name': 'Катлета Флингера',
-		'usage': 'ТАК СЛОЖНО НАПИСАТЬ <code>(Ты еблан).help Жаконизатор</code> , ДОЛБАЕБ?',
-	}
-	def __init__(self):
-		self.name = self.strings['name']
-		self._me = None
-		self._ratelimit = []
-	async def client_ready(self, client, db):
-		self._db = db
-		self._client = client
-		self.me = await client.get_me()
-		
-	async def jcmd(self, message):
-		""".j <реплай на сообщение/свой текст>\n@floodmemeframe подпишись-не пожалеешь"""
-		
-		ufr = requests.get("http://allfont.de/cache/fonts/open-sans_5f14bd2f3cd41e7b13ff8bc4177c9d06.ttf")
-		f = ufr.content
-		
-		reply = await message.get_reply_message()
-		args = utils.get_args_raw(message)
-		if not args:
-			if not reply:
-				await utils.answer(message, self.strings('usage', message))
-				return
-			else:
-				txt = reply.raw_text
-		else:
-			txt = utils.get_args_raw(message)
-		await message.edit("<b>Заебеним цитатку ...</b>")
-		pic = requests.get("https://raw.githubusercontent.com/FlinGer-TermuX/FlinGerFTG_Modules/master/zhak-fresko-memy-1.jpg")
-		pic.raw.decode_content = True
-		img = Image.open(io.BytesIO(pic.content)).convert("RGB")
- 
-		W, H = img.size
-		#txt = txt.replace("\n", "𓃐")
-		text = "\n".join(wrap(txt, 19))
-		t = text + "\n"
-		#t = t.replace("𓃐","\n")
+import ")
 		draw = ImageDraw.Draw(img)
 		font = ImageFont.truetype(io.BytesIO(f), 32, encoding='UTF-8')
 		w, h = draw.multiline_textsize(t, font=font)
@@ -66,3 +17,4 @@ class JacquesMod(loader.Module):
 		out.seek(0)
 		await message.client.send_file(message.to_id, out, reply_to=reply)
 		await message.delete()
+Иги
