b21b97281f635909c86d78bbc1ef01f683b49032
FlinGer-TermuX <67793657+FlinGer-TermuX@users.noreply.github.com>
2020-08-24 16:59:57 +0500

    Update arts.py

--- arts.py
+++ arts.py
@@ -6,8 +6,8 @@ def register(cb):
 
 
 class ArtsMod(loader.Module):
-    """Юникод арты"""
-    strings = {'name': 'UnicodeArts'}
+    """ASCII арты"""
+    strings = {'name': 'ASCII Arts'}
 
     async def vjuhcmd(self, message):
         """Используй .vjuh <текст> c:"""
