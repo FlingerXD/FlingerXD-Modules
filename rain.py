72d695408507d69077564d008b77605117a9fb9c
FlinGer-TermuX <67793657+FlinGer-TermuX@users.noreply.github.com>
2020-09-14 11:07:06 +0500

    Upload file

---
+++ rain.py
@@ -0,0 +1,15 @@
+# Originally created By KingMars
+
+from telethon import events
+import asyncio
+from collections import deque
+
+@borg.on(events.NewMessage(pattern=r"\.rain", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    deq = deque(list("☁️☁️🌧🌩⛈🌨🌪🌪🌊"))
+    for _ in range(100):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
