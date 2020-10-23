fd3abb7994d2cc5cac3315e749ae00506cd07f0c
FlinGer <67793657+FlinGer-TermuX@users.noreply.github.com>
2020-10-23 16:41:25 +0500

    Upload file

---
+++ poops.py
@@ -0,0 +1,31 @@
+from telethon import events
+import asyncio
+import os
+import sys
+
+
+@borg.on(events.NewMessage(pattern=r"\.poops", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+       
+ 
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("💩💩💩💩 \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n💩💩💩💩 \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n💩💩💩💩 \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n💩💩💩💩 \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n💩💩💩💩 \n")
+    await asyncio.sleep(1)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n💥💥💥💥 \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n💥💥💥💥 \n💥💥💥💥 \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n🤮🤮🤮🤮 \n")
+    await asyncio.sleep(0.5)
+    await event.edit("ОТ ТЕБЯ ГАВНОЙ ВОНЯЕТ, ЧЕРЕЗ ТЕЛЕФОН СЛЫШУ, НЕ ПИШИ СЮДА БОЛЬШЕ БЛЯТЬ!")
