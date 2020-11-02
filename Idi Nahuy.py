136fa79e73db762055b91bd1e9cc5288974cd992
FlinGer <67793657+FlinGer-TermuX@users.noreply.github.com>
2020-11-02 18:05:35 +0500

    Update Idi Nahuy.py

--- Idi Nahuy.py
+++ Idi Nahuy.py
@@ -26,7 +26,7 @@ class NahuyMod(loader.Module):
         if utils.get_args_raw(message):
             self._db.set(__name__, "afk", utils.get_args_raw(message))
         else:
-            self._db.set(__name__, "afk", иди нахуй!)
+            self._db.set(__name__, "afk", True)
         self._db.set(__name__, "gone", time.time())
         await self.allmodules.log("afk", data=utils.get_args_raw(message) or None)
         await utils.answer(message, self.strings("gone", message))
